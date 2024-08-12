9c667691f23bc05ae31296b06e27d087304e8803
nishant08-05 <rajnishant0804@gmail.com>
2024-08-13 00:22:29 +0530

    Add files via upload

---
+++ addsub.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int first, second, add, subtract, multiply;
+    float divide;
+    
+    cout << "Please enter two integer: ";
+    cin >> first;
+    cin >> second;
+    
+    add        = first + second;
+    subtract = first - second;10
+    
+    cout << endl <<"Sum = " << add;
+    cout << endl <<"Difference = " << subtract; 
+    
+    return 0;
+}
